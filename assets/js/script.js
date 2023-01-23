e97af76154cd35c5866130df2b3078d68b5c2f24
codri5 <97296166+codri5@users.noreply.github.com>
2023-01-23 17:22:51 +0000

    Added radios

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ let display = document.querySelector('.display');
 let hide = document.querySelector('#start-quiz');
 let startBtn = document.querySelector('#start');
 let nextBtn = document.querySelector('#next');
+let radio1 = document.getElementById('rad1');
+let radio2 = document.getElementById('rad2');
+let radio3 = document.getElementById('rad3');
 
 // Shuffle array
 let questionsChosen = [];
@@ -27,19 +30,31 @@ let correctAnswers = [];
 questions.forEach(item => correctAnswers.push(item.answer));
 correctAnswers = correctAnswers.slice(0, 5);
 
+// Add answer options to radios
+function radioContent() {
+    radio1.textContent = answerOptions[count][0];
+    radio2.textContent = answerOptions[count][1];
+    radio3.textContent = answerOptions[count][2];
+}
+
 // Display random question
 let count = 0;
 
 function displayQuestion() {
   titles.textContent = questionTitles[count];
+  radioContent()
+
   display.setAttribute('style', 'display: inline');
   hide.setAttribute('style', 'display: none');
+  titles.setAttribute('style', 'font-size: 100%');
+  nextBtn.setAttribute('style', 'font-size: 70%');
 }
 
 function nextQuestion() {
     count++;
     titles.textContent = questionTitles[count];
-  }
+    radioContent()
+}
 
 startBtn.addEventListener('click', displayQuestion);
 nextBtn.addEventListener('click', nextQuestion);
